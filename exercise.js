70e207472acca2e55b27de8b7245259ab9e15bc9
Alexandra Sirois <AlexandraSirois@Alexandras-MacBook-Pro.local>
2017-06-12 15:48:56 -1000

    completed #4

--- exercise.js
+++ exercise.js
@@ -9,11 +9,14 @@
 
 var longestPlaceName = "Taumatawhakatangihangakoauauotamateaturipukakapiki- maungahoronukupokaiwhenuakitanatahu";  //Yes, this is a real place located in Porangahau, Central Hawke's Bay.
 
-
+var howManyLetters = longestPlaceName.length
+console.log(howManyLetters);
 
 /*Next, concatenate and console.log the following phrase:
 "x is the longest place name in the world and has y letters in its name."
 where x represents the value at `longestPlaceName` and y represents the value at `howManyLetters`*/
+
+console.log(longestPlaceName + " is the longest place name in the world and has " + howManyLetters + " letters in its name.");
 
 
 /*The string.charAt() method returns the character at the specified index (position).*/
@@ -22,7 +25,8 @@ where x represents the value at `longestPlaceName` and y represents the value at
 
 var iDidntKnow = "The national animal of Scotland is the Unicorn";
 
-
+var fifthLetter = iDidntKnow.charAt(5);
+console.log(fifthLetter);
 
 /*The indexOf() method returns the index (position) of the first occurence of a specified text in a string.*/
 
@@ -30,7 +34,8 @@ var iDidntKnow = "The national animal of Scotland is the Unicorn";
 
 var randomFact = "All pandas in the world are on loan from China.";
 
-
+var worldLocator = randomFact.indexOf("world");
+console.log(worldLocator);
 
 /*The slice() method extracts a part of a string and returns the extracted part into a new string. This method takes 2 parameters: the starting index (position) and the ending index (position)*/
 
@@ -38,7 +43,8 @@ var randomFact = "All pandas in the world are on loan from China.";
 
 var edward = "People are afraid of me because I am different";
 
-
+var scissorHand = edward.slice(7, 17);
+console.log(scissorHand);
 
 /*The replace() method replaces a specified value with another value in a string.*/
 
